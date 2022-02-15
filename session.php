e28c11698689cdffa587c471e5451b6962ec592d
Sven Blanke <sven.blanke@software-architects.de>
2022-02-15 18:55:17 +0100

    missing apostrophs

--- session.php
+++ session.php
@@ -7,7 +7,7 @@ updatePageViews($_GET["id"], ip2long($_SERVER['REMOTE_ADDR']));
 
 echo "{\"initiatives\": ".$settings["initiatives"].
     ", \"mapping\": ".$settings["mapping"].
-    ", \"user_name\": ".$settings["user_name"].
-    ", \"user_role\": ".$settings["user_role"].
+    ", \"user_name\": \"".$settings["user_name"]."\"".
+    ", \"user_role\": \"".$settings["user_role"]."\"".
     "}";
 ?>
