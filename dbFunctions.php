066cda5caa3e93519f40fd1d0a58f37d4a6460b1
Sven Blanke <sven.blanke@software-architects.de>
2022-02-17 02:12:27 +0100

    read email

--- dbFunctions.php
+++ dbFunctions.php
@@ -46,7 +46,7 @@ function updatePageViews($sessionId, $remoteIp) {
 
 function readSettings($id) {
 	$conn = dbInit();
-	$sql = "SELECT id, initiatives, mapping, user_name, user_role FROM settings WHERE id = "
+	$sql = "SELECT id, initiatives, mapping, user_name, user_role, email FROM settings WHERE id = "
 		. mysqli_real_escape_string($conn, $id);
 	$result = $conn->query($sql);
 	$conn->close();
